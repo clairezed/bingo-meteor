e554d21011fa89d3c20d0d6664a6003f7fd4a417
clairezed <clairezuliani@gmail.com>
2014-03-13 19:09:49 +0100

    #20 array words in Games, addWord and removeWord working

--- client/views/application/words/add_words.js
+++ client/views/application/words/add_words.js
@@ -1,36 +1,36 @@
 Template.addWords.helpers({
-    words: function(){
-        return Words.find({game_id: this._id});
-    },
-    enough_words: function(){
-        game = Games.findOne(this._id);
-        return game.nb_words >= game.nb_words_required;
-    }
+    // words: function(){
+    //     return Words.find({game_id: this._id});
+    // }
 });
 
 Template.addWords.events({
     'submit form': function(e, template){
         e.preventDefault();
 
-        var word = {
-            word: $(e.target).find('[name=word]').val(),
-            game_id: template.data._id
-        }
+        // var word = $(e.target).find('[name=word]').val();
+        // var game = Games.findOne({_id: template.data._id})
+        var args = {};
+        args["game_id"] =  template.data._id;
+        args["word"] = $(e.target).find('[name=word]').val();
 
-        Meteor.call('createWord', word, function(error, current_game_id){
+        Meteor.call('createWord', args, function(error, current_game_id){
             if (error)
                 throwError(error.reason);
-            // var current_game = Games.findOne({_id: template.data._id})
+            
             //// clear input text
             $(e.target).find('[name=word]').val("");
-            
-            Router.go('addWords', {_id: current_game_id});
         })   
     },
-    'click .delete': function(e){
-        var word = this;
+    'click .delete': function(e, template){
+        console.log($(e.target).prev().html());
+        var word = $(e.target).prev().html();
+
+        var args = {};
+        args["game_id"] =  template.data._id;
+        args["word"] = $(e.target).prev().html();
        
-        Meteor.call('deleteWord', word, function(error){
+        Meteor.call('deleteWord', args, function(error){
             if(error)
                 throwError(error.reason);
         })
--- collections/games.js
+++ collections/games.js
@@ -27,15 +27,13 @@ Meteor.methods({
 				"Sorry Dude, a bingo game with the same title already exists", 
 				gameWithSameTitle._id);
 		}
-
-
 		var game = _.extend(_.pick(gameAttributes, 'title'), {
 			creatorId: user._id,
 			created_at: new Date().getTime(),
 			updated_at: new Date().getTime(), 
-			nb_words: 0,
+			words: [],
 			nb_words_required: 5,
-			ready: this.nb_words > this.nb_words_required
+			ready: false
 		});
 
 		var gameId = Games.insert(game);
@@ -46,5 +44,31 @@ Meteor.methods({
 		var game = Games.findOne(gameId);
 		Words.remove({game_id: game._id});
 		Games.remove(game._id);
-	}
+	},
+	// args : game_id / word
+	createWord: function(args){
+		var user = Meteor.user();
+		if(!user)
+				throw new Meteor.Error(401, "You need to login to add a word, sweetie");
+		if(args.word =="")
+				throw new Meteor.Error(422, "Aren't you trying to add a blank tile ?");
+
+		var game = Games.findOne(args.game_id);
+		var ready = game.words.length + 1 >= game.nb_words_required;
+
+		Games.update(game._id, {
+			$set: {ready: ready}, 
+			$addToSet: {words: args.word}
+		});
+		return game._id;
+	}, 
+	deleteWord: function(args){
+		var game = Games.findOne(args.game_id);
+		// check if still enough words to play bingo
+		var ready = game.words.length - 1 >= game.nb_words_required;
+        	Games.update(game._id, {
+        		$set: {ready: ready}, 
+        		$pull: {words: args.word}
+        	});
+	},
 })
