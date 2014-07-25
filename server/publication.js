a650e4afc2eb0302a13e9a8fb313eac5414978d3
clairzed <clairezuliani@gmail.com>
2014-07-26 01:22:19 +0200

    #33 clean grid subscription

--- server/publication.js
+++ server/publication.js
@@ -1,48 +1,48 @@
 // GRIDS
 Meteor.publish('grid', function(gridId) {
-    return Grids.find({_id: gridId});
+  return Grids.find({_id: gridId});
 });
 
-Meteor.publish('grids', function() {
-    return Grids.find();
+Meteor.publish('readyGrids', function() {
+  return Grids.find({ready: true});
 });
+
+Meteor.publish('userGrids', function(userId) {
+  return Grids.find({'creator.id': userId});
+})
 
 
 // GAMES
 
-Meteor.publish('games', function() {
-    return Games.find();
-});
-
 Meteor.publish('visibleGames', function() {
-    return Games.find({preview: false});
+  return Games.find({preview: false});
 });
 
 Meteor.publish('game', function(gameId) {
-    console.log("game startObservers");
-    Games.startObservers(gameId);
-    this.onStop(function() {
-        console.log("game stopObservers");
-        Games.stopObservers(gameId);
-    })
-    return Games.find({_id: gameId});
+  console.log("game startObservers");
+  Games.startObservers(gameId);
+  this.onStop(function() {
+    console.log("game stopObservers");
+    Games.stopObservers(gameId);
+  })
+  return Games.find({_id: gameId});
 });
 
 Meteor.publish('userGames', function(userId) {
-    return Games.find({creatorId: userId});
+  return Games.find({creatorId: userId});
 })
 
 
 // PLAYER CONTENTS
 
 Meteor.publish('playerContents', function(gameId) {
-    console.log("pc startObservers");
-    PlayerContents.startObservers(gameId);
-    this.onStop(function () {
-        console.log("pc stopObservers");
-        PlayerContents.stopObservers(gameId);
-    });
-    return PlayerContents.find({gameId: gameId});
+  console.log("pc startObservers");
+  PlayerContents.startObservers(gameId);
+  this.onStop(function () {
+    console.log("pc stopObservers");
+    PlayerContents.stopObservers(gameId);
+  });
+  return PlayerContents.find({gameId: gameId});
 });
 
 // Meteor.publish('player_found_contents', function () {
@@ -57,22 +57,22 @@ Meteor.publish('playerContents', function(gameId) {
 
 Meteor.publish('bingoActivities', function (gameId) {
   Meteor.call('clearActivity', function(error){
-        if(error)
-            console.log(error.reason);
-    })
+    if(error)
+      console.log(error.reason);
+  })
 
-    return BingoActivities.find({game_id: gameId});
+  return BingoActivities.find({game_id: gameId});
 });
 
 
 // USERS
 
 Meteor.publish('players', function (gameId) {
-    return Meteor.users.find({playing: gameId}, {fields: {playing: 1, username: 1,}});
+  return Meteor.users.find({playing: gameId}, {fields: {playing: 1, username: 1,}});
 });
 
 Meteor.publish('gamePlayers', function(userIds) {
-    return Meteor.users.find({_id: {$in: userIds}});
+  return Meteor.users.find({_id: {$in: userIds}});
 });
 
 
