20343d8121c5c5ac3d92a3e0b4ca33380c9b2c70
clairzed <clairezuliani@gmail.com>
2014-07-26 01:24:01 +0200

    #34 fix grid margin

--- client/views/userspace/userspace.js
+++ client/views/userspace/userspace.js
@@ -1,23 +1,23 @@
 Template.userspace.helpers({
-    grids: function(){
-        var user = Meteor.user();
-        if (user) {
-            return Grids.find({'creator.id': user._id});
-        }
-    }
+  grids: function(){
+    return Grids.find({'creator.id': Meteor.userId()}, {sort: {creationDate:-1}, limit:4}).map(function(grid, index) {
+      grid.position = index;
+      return grid;
+    });
+  }
 });
 
 Template.userspace.events({
-    'click .delete-game': function(e){
-        e.preventDefault();
+  'click .delete-game': function(e){
+    e.preventDefault();
 
-        if(confirm("Do you really want to delete your sweet bingo, sugar ?")){
-            var currentGridId = this._id;
+    if(confirm("Do you really want to delete your sweet bingo, sugar ?")){
+      var currentGridId = this._id;
 
-            Meteor.call('deleteGrid', currentGridId, function(error){
-                if (error)
-                    throwError(error.reason);
-            })
-        }
+      Meteor.call('deleteGrid', currentGridId, function(error){
+        if (error)
+          throwError(error.reason);
+      })
     }
+  }
 })
