116262392860ee16b4005920cd62b1e262664257
clairzed <clairezuliani@gmail.com>
2014-07-26 00:25:13 +0200

    #45 show me more btn

--- client/views/allBingos/allBingos.js
+++ client/views/allBingos/allBingos.js
@@ -1,5 +1,8 @@
 Template.allGridsList.helpers({
   readyGrids: function(){
-    return Grids.find({ready: true});
+    return Grids.find({ready: true}, {sort: {creationDate:-1}}).map(function(grid, index) {
+      grid.position = index;
+      return grid;
+    });
   }
 });
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,9 +1,12 @@
 Template.latestGridsList.helpers({
-  readyGrids: function(){
+  readyGrids: function() {
     return Grids.find({ready: true}, {sort: {creationDate:-1}, limit:4}).map(function(grid, index) {
       grid.position = index;
       return grid;
     });
+  },
+  moreGrids: function() {
+    return Grids.find({ready: true}).fetch().length > 4;
   }
 });
 
