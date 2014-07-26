88e6c9b3f8c26541ba26fffb072444ca4f1c99c8
clairzed <clairezuliani@gmail.com>
2014-07-26 16:24:48 +0200

    #48 delete grid

--- client/views/grids/sharedGrid/configGrid.js
+++ client/views/grids/sharedGrid/configGrid.js
@@ -1,4 +1,8 @@
 Template.configGrid.helpers({
+  onUpdatePage: function() {
+
+    return this._id;
+  },
   hasPublicVisibility: function() {
     if(grid = Grids.findOne({_id: this._id})){
       return grid.visibility == "public";
@@ -8,5 +12,21 @@ Template.configGrid.helpers({
     if(grid = Grids.findOne({_id: this._id})){
       return grid.visibility == "private";
     };
+  }
+})
+
+Template.configGrid.events({
+  'click .delete-grid': function(e, template) {
+    e.preventDefault();
+    var gridId = template.data._id;
+
+    Meteor.call('deleteGrid', gridId, function(error){
+      if(error) {
+        throwError(error.reason);
+      } else {
+        console.log("grid deleted");
+        Router.go('home');
+      }
+    });
   }
 })
