dd957e995de5001e21a72f769060d5b0b9ac2f36
clairzed <clairezuliani@gmail.com>
2014-08-04 22:18:14 +0200

    #40 soft security check

--- client/views/shared/launch_game_btn.js
+++ client/views/shared/launch_game_btn.js
@@ -9,29 +9,33 @@ Template.launchGameBtn.helpers({
 Template.launchGameBtn.events({
   'click .launch-game': function(e, template) {
     e.preventDefault();
-    var gridId = template.data._id;
-    var game;
-    if (game = Games.findOne({'creator.id': Meteor.userId(), gridId: gridId})) {
-      Meteor.call('launchGame', game._id, function(error, gameId){
-          if (error){
-              throwMessage(error.reason, 'danger');
-          }else{
-              Router.go('playGame', {_id: gridId, gameId: gameId});
-          }
-      });
+    if(!Meteor.user()) {
+      Router.go('authenticate');
     } else {
-      var game = {
-          gridId: gridId,
-          preview: false
+      var gridId = template.data._id;
+      var game;
+      if (game = Games.findOne({'creator.id': Meteor.userId(), gridId: gridId})) {
+        Meteor.call('launchGame', game._id, function(error, gameId){
+            if (error){
+                throwMessage(error.reason, 'danger');
+            }else{
+                Router.go('playGame', {_id: gridId, gameId: gameId});
+            }
+        });
+      } else {
+        var game = {
+            gridId: gridId,
+            preview: false
+        }
+        console.log("createGame from launchGameBtn click launch game, else")
+        Meteor.call('createGame', game, function(error, gameId){
+            if (error){
+                throwMessage(error.reason, 'danger');
+            }else{
+                Router.go('playGame', {_id: gridId, gameId: gameId});
+            }
+        });
       }
-      console.log("createGame from launchGameBtn click launch game, else")
-      Meteor.call('createGame', game, function(error, gameId){
-          if (error){
-              throwMessage(error.reason, 'danger');
-          }else{
-              Router.go('playGame', {_id: gridId, gameId: gameId});
-          }
-      });
     }
   }
 })
