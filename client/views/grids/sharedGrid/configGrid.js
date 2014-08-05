eb4f20b06e15aafe8fd6474ba84f90cc15d6e1ea
clairzed <clairezuliani@gmail.com>
2014-08-05 21:00:17 +0200

    #47 #56 popover working example and public visibiity automated check

--- client/views/grids/sharedGrid/configGrid.js
+++ client/views/grids/sharedGrid/configGrid.js
@@ -4,7 +4,9 @@ Template.configGrid.helpers({
     return this._id;
   },
   hasPublicVisibility: function() {
-    if(grid = Grids.findOne({_id: this._id})){
+    if(!this._id) {
+      return true;
+    }else if(grid = Grids.findOne({_id: this._id})){
       return grid.visibility == "public";
     };
   },
@@ -30,3 +32,20 @@ Template.configGrid.events({
     });
   }
 })
+
+var helpData = {
+  'visibility-help': {
+    title: "About visibility ",
+    message: "<strong>Public visibility :</strong> everyone can see your bingo. <br> <strong>Private visibility :</strong> only you, and people who have a direct link to your bingo, can see it.", //supports Markdown
+    // url: "http://YOUR_URL_TO_ADDITIONAL_HELP",
+    options: {
+      placement: 'right'
+    }
+  },
+  'another-help-name': {
+    title: "another help document title ", //supports Markdown
+    message: "another help document message",
+    url: "http://YOUR_URL_TO_ADDITIONAL_HELP"
+  },
+}
+InlineHelp.initHelp(helpData);
